9d8e3376a1a6184a734a1e33a67c65215be64917
ClG <cg@jmg-conseil.eu>
2023-02-15 16:14:46 +0100

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {Button} from "flowbite-svelte";
+    import {goto} from "$app/navigation";
+    function redirect(path){
+        goto(path);
+    }
+</script>
+
+<div class="h-screen">
+    <h1>Dépôt de fichiers</h1>
+   <Button on:click={goto("/ipfs")}>Déposer un fichier</Button>
+    <Button on:click={goto("/about")}>À propos de ce service</Button>
+    <Button on:click={goto("/ipfs")}>Mon compte</Button>
+    <Button on:click={goto("/ipfs")}>Paramètres</Button>
+</div>
+
---
+++ src/routes/pages/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-4xl">À propos de ce service</h1>
